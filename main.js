f3c5a054031993005261dc4defd60616f9609296
Yuuki Furuyama <addsict@gmail.com>
2018-01-28 20:30:08 +0900

    implement

--- main.js
+++ main.js
@@ -7,26 +7,63 @@
     bindEvent() {
       const peek = this.peek.bind(this);
       window.addEventListener('mouseup', function(e) {
-        const selected = window.getSelection().toString();
+        const selection = window.getSelection();
+        const selected = selection.toString();
         if (selected.length == 0) {
           return;
         }
-        peek(selected);
+        const position = selection.getRangeAt(0).getBoundingClientRect();
+        peek(selected, position);
       }, false);
     }
 
-    peek(str) {
+    peek(str, position) {
       const decoded = this._decodeJWT(str);
       if (decoded === null) {
         console.log("not valid jwt: " + str);
         return;
       }
-
       const header = decoded[0];
       const payload = decoded[1];
 
-      console.log("header: " + JSON.stringify(header, null, 2));
-      console.log("payload: " + JSON.stringify(payload, null, 2));
+      // setup element for tooltip
+      let toolTipInsertDiv = document.createElement('div');
+      const relative = document.body.parentNode.getBoundingClientRect();
+      const positionLeft = position.left + (-relative.left) + 100; // 100 is adhoc buffer
+      const positionTop = position.top + (-relative.top);
+      toolTipInsertDiv.style = `position: absolute; left: ${positionLeft}px; top: ${positionTop}px;`;
+      document.body.appendChild(toolTipInsertDiv);
+
+      // wait for toolTipInsertDiv being appended
+      setTimeout(function() {
+        let toolTipContentDiv = document.createElement('div');
+        const headerContent = JSON.stringify(header, null, 4);
+        const payloadContent = JSON.stringify(payload, null, 4);
+        toolTipContentDiv.innerHTML = `
+          <div class="peek-jwt--container">
+            <div class="peek-jwt--container--item">
+              <span class="peek-jwt--container--item--heading">HEADER</span>
+              <span><pre class="peek-jwt--container--item--code">${headerContent}</pre></span>
+            </div>
+            <div class="peek-jwt--container--item">
+              <span class="peek-jwt--container--item--heading">PAYLOAD</span>
+              <span><pre class="peek-jwt--container--item--code">${payloadContent}</pre></span>
+            </div>
+          </div>
+        `;
+        
+        const tip = tippy(toolTipInsertDiv, {
+          trigger: 'manual',
+          interactive: true,
+          arrow: true,
+          html: toolTipContentDiv,
+          onHidden: function() {
+            toolTipInsertDiv._tippy.destroy();
+          },
+          theme: 'peek-jwt bordered',
+        });
+        toolTipInsertDiv._tippy.show();
+      }, 0);
     }
 
     _decodeJWT(str) {
